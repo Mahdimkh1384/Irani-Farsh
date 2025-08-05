00a7bca2fb1973b37ea0cbd319774d8bc6035ca2
Mahdi-Marami <mramym68@gmail.com>
2025-08-05 21:50:12 +0330

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import NavBar from "@/Components/NavBar/NavBar";
+import Footer from "@/Components/Footer/Footer";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
