06cc7460d52fd10eee3fca0a60d6e10b4379750c
Mahdi-Marami <mramym68@gmail.com>
2025-08-06 13:26:04 +0330

    some changes and install packages

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css"
 import NavBar from "@/Components/NavBar/NavBar";
 import Footer from "@/Components/Footer/Footer";
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
