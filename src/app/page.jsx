20fb769acf8716dd63319036ad2bcbedb3c0902c
Mahdi-Marami <mramym68@gmail.com>
2025-08-08 20:12:48 +0330

    create categoryBox component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,17 @@
 import Landing from "@/Components/Landing/Landing";
 import SectionHeader from "@/Components/SectionHeader/SectionHeader";
-import ProductBox from "@/Components/ProductBox/ProductBox";
+import CategoryBox from "@/Components/CategoryBox/CategoryBox";
 import Slider from "@/Components/Slider/Slider";
 
 export default function Home() {
   return (
     <>
       <Landing />
+      <CategoryBox />
       <SectionHeader title="جدیدترین ها" />
+      <Slider />
+      <CategoryBox />
+      <SectionHeader title="فرش های ماشینی" />
       <Slider />
     </>
   );
