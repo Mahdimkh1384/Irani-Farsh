0148f3f13920a85cdd57a94a958c645c762bbd9a
Mahdi-Marami <mramym68@gmail.com>
2025-08-08 11:36:52 +0330

    responsive landing component

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Landing() {
     return (
-        <div className=' pr-[108px] pl-[108px] mt-20 h-[470.9px] flex justify-between '>
-            <div className=' mt-24 w-[600px] h-[280px] flex flex-col items-center'>
-                <h2 className='text-3xl'>خرید راحت فرش،در<span className='text-primary'>ایرانی فرش</span></h2>
-                <p className='text-center font-[Rokh-light] font-bold text-[24px] mt-6 w-[590px]'>با ایـــرانی فرش در سریع ترین زمان ممکن فرش خودت رو سفارش بده و از تنوع بی نظیر فرش ها لذت ببر.</p>
-                <div className='flex gap-x-10 mt-12'>
-                    <button className='btn h-14 w-[206px]'>محصولات ویژه ایرانی فرش</button>
-                    <button className='font-bold font-[Rokh-light] bg-primary text-white border rounded-[12px] flex justify-center items-center hover:cursor-pointer hover:bg-red-700 transition-colors h-14 w-[206px]'>عضویت در باشگاه مشتریان</button>
+        <div className=' lg:pr-[108px] lg:pl-[108px] lg:mt-20 sm:mt-16 h-[480.9px]  flex sm:flex-col-reverse lg:flex-row lg:justify-between  sm:justify-center sm:items-center '>
+            <div className=' lg:mt-3 sm:mt-7 lg:w-[600px] lg:h-[280px] sm:w-[328px] sm:h-[144px] flex flex-col items-center'>
+                <h2 className='lg:text-3xl sm:text-2xl'>خرید راحت فرش،در<span className='text-primary'>ایرانی فرش</span></h2>
+                <p className='text-center font-[Rokh-light] font-bold lg:text-[24px] sm:text-[12px] mt-6 lg:w-[590px] sm:w-[310px]'>با ایـــرانی فرش در سریع ترین زمان ممکن فرش خودت رو سفارش بده و از تنوع بی نظیر فرش ها لذت ببر.</p>
+                <div className='flex w-full justify-center lg:gap-x-10 sm:gap-x-4 lg:mt-12 sm:mt-6 lg:h-14 sm:h-8 '>
+                    <button className='btn lg:h-14 sm:h-10 lg:w-[206px] sm:w-[152px] lg:text-[18px] sm:text-[14px]'>محصولات ویژه ایرانی فرش</button>
+                    <button className='font-bold lg:text-[18px] sm:text-[14px] font-[Rokh-light] bg-primary text-white border rounded-[12px] flex justify-center items-center hover:cursor-pointer hover:bg-red-700 transition-colors lg:h-14 sm:h-10 lg:w-[206px] sm:w-[152px]'>عضویت در باشگاه مشتریان</button>
                 </div>
             </div>
-            <img className='w-[527px] h-[470px]' src="./images/group 3.png" alt="carpets" />
+            <img className='lg:w-[527px] lg:h-[470px] sm:w-[280px] sm:h-[250px]' src="./images/group 3.png" alt="carpets" />
         </div>
     )
 }
