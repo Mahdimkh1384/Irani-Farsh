5b0f16c335402d3cfbb449ea59654b3dab27aca1
Mahdi-Marami <mramym68@gmail.com>
2025-08-11 13:08:35 +0330

    fix buttons bug in 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function NotFound() {
@@ -5,7 +6,7 @@ export default function NotFound() {
         <div className='lg:px-[108px] sm:px-3 flex flex-col items-center gap-y-4 lg:mt-2 sm:mt-10'>
             <img className='lg:w-[700px]  lg:h-[450px] sm:h-[350px] sm:w-[600px]' src="/images/not-found.png" alt="not found" />
             <h1 className='text-4xl font-[Rokh-light] font-bold'>صفحه مورد نظر پیدا نشد</h1>
-            <button className='btn w-[200px] h-[40px]'>صفحه اصلی</button>
+            <Link href="/" className='btn w-[200px] h-[40px]'>صفحه اصلی</Link>
         </div>
     )
 }
