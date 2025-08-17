d1973afe5d2154cee5c76024a74455bdf5f7918a
Mahdi-Marami <mramym68@gmail.com>
2025-08-17 21:57:54 +0330

    fix bug in not found page

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,16 +1,13 @@
-import "./globals.css"
 import NavBar from "@/Components/NavBar/NavBar";
 import Footer from "@/Components/Footer/Footer";
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" dir="rtl">
-      <body>
-        <NavBar />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <NavBar />
+      {children}
+      <Footer />
+    </>
   );
 }
