9ef969c931c6b7e1e0acaa60f637f6cc491ec9f4
Sajjad4297 <sajjad.ranjbaran.4297@gmail.com>
2025-12-11 02:15:53 +0330

    config updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,24 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'api.iranifarsh.neofy.ir',
-            port: '',
-            pathname: '/uploads/**',
-            search: '',
-          }
+            {
+                protocol: 'https',
+                hostname: 'api.iranifarsh.neofy.ir',
+                pathname: '/uploads/product/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.iranifarsh.neofy.ir',
+                pathname: '/uploads/category/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.iranifarsh.neofy.ir',
+                pathname: '/uploads/user/**',
+            },
+
         ],
-      }
-    };
+    }
+};
 
 export default nextConfig;
